b6d36d040b91124ac270eb0293aa6a659f347483
Sneha Purkayastha <iamsneha0296@gmail.com>
2023-06-09 08:09:43 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Skills from './components/Skills';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route/>
-          <Route/>
-          <Route/>
-          <Route/>
-          <Route/>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/skills' element={<Skills/>}/>
+          <Route path='/projects' element={<Projects/>}/>
+          <Route path='/contact' element={<Contact/>}/>
         </Routes>
       </BrowserRouter>
       {/* <Footer/> */}
